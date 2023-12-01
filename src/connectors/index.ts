aad520d1e646f3cec827643f30a5c62221028926
lihang941 <1329075357@qq.com>
2023-12-01 15:32:02 +0800

    feat: swap

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -19,7 +19,7 @@ export const network = new NetworkConnector({
 })
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 11155111]
+  supportedChainIds: [ 11155111]
 })
 
 // mainnet only
